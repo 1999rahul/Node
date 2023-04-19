e857f57c03c61f99c9fdb1df16a5a2b0629e13e8
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-04-19 11:54:40 +0530

    Http part 1

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 /*
--In Node we have a global object and everything in this global object can be accessed from anywere.
-- when we define a variable it does not get assosiated with the global object.
+We use Http module to send Http request and make backend servers.
+- Http module has a method called createServer which is an extends from EventEmmiter class, so we can use it to handle request events.
 */
+
+const http = require('http')
+const server = http.createServer();
+
+ /*
+ - when a new request is received to server object, it emits an event named connection, so we can listen to it using on function.
+ */
+
+ server.on('connection',(socket)=>{
+    console.log("connection")
+ })
+
+ server.listen(3000)
+ console.log('Listning on port 3000')
