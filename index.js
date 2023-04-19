e9caad306135e9f3387a0592ed1d62d7f187c5ed
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-04-19 15:22:11 +0530

    7.Route Parameters

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/api/courses',(req,res)=>{
     res.send([1,2,3,4,5])
 })
 
+
+//We use route parameters for required values, route parameters are stored in an object called parms and can be accessed using req,parms
+//we use query string parameters for optional values, query string can be accessed using req.query.req.query is an object.
+//query parameter can be sent as http://localhost:3000/api/courses/10/20?name=Design and Analysis of Algorithms&price=6000
+app.get('/api/posts/:year/:month',(req,res)=>{
+    res.send({query:req.query,params:req.params})
+})
+
 //An enviroment variable is the variable which is the part of env. in which a process runs.
 //we can acess env variable using process object.
 const port=process.env.PORT || 3000
