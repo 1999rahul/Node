75933584e2aebc73aeb978a3c9794ce0a43300e2
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-04-19 11:25:23 +0530

    fs module

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const EventEmmitter =require('events')
-
-const Logger=require('./logger')
-
-const logger=new Logger()
-
-logger.on("messegeLogged",(arg)=>{
-    console.log('Listner called',arg)
+const fs=require('fs')
+//we should allaways use async functions
+//remember javascript has only one thread so we should allaways use async functions.
+fs.readdir('./',(err,files)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(files)
+    }
 })
-
-logger.log('messege')
