c4a3b2620ab8b8ed90de22a1abf553a1dae12728
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-04-20 16:55:53 +0530

    running promises in parrlel

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 /*
-   * Javascript is a single threaded language, so we must write non blocking code.
-   * Async statements are just like a resturent with a simgle waiter.
-   * In javascript we have three patterns to acheive the async behaviour in code, Callbacks, promises, Async/Await
+ * Sometimes we need to call multiple async operations all at once and without dependency
+ * In this case we use Promise.all() which takes array of promise which has to be fulfilled.
 */
-/*
- * using callbacks for handling async operations leads to nested and unreadable code so we will avoid using callbacks.
- * we will use promises or async/await for handling async operations.
- * we chain the async operations to get the clean structure.
-*/
-console.log("Before")
-const p=getUser(10)
-                  .then((user)=>{getRepos(user.userName)})
-                  .then((repos)=>{console.log(repos)})
-                  .catch(err=>{console.log(err)})
 
-console.log("After")
+const p1=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("Async Operation 1")
+        resolve({id:1})
+    },2000)
+})
 
-function getUser(id){  
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log("reading user from DB...")
-            resolve({id:id,userName:'myName'})
-        },2000)
-    })
-    
-}
+const p2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("Async Operation 2")
+        resolve({id:2})
+    },2000)
+})
 
-function getRepos(userName){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log(`Getting repos of ${userName}`)
-            resolve(['repo 1','repo 2','repo 3'])
-      },2000)
-    })
-}
+Promise.all([p1,p2]).then((res)=>{
+    console.log(res)
+}).catch((err)=>{
+    console.log(err)
+})
