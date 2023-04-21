bfbe9423fcc0d8b97e1da71599027792bc2f9ede
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2023-04-21 14:25:58 +0530

    winston module

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ require('express-async-errors')
 const express=require('express')
 const courses=require('./routes/courses')
 const error = require('./middlewares/error')
+const winston=require('winston')
 const app=express()
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  })
 
 /**
  * We can use try catch block to handle the exception but this is very repetative task.
@@ -41,11 +51,20 @@ function asyncErrorHandlerMiddleware(handler){
  * express-async-errors module encapulates the logic of asyncErrorHandlerMiddleware and we also do not need to call any function in our routes.
  * if this module does not works then we should use the previous approch.
  */
-app.use(express.json())
+
+/**
+ * To log the data we can use winstion module.
+ * Using this module we can use different methods to log messeges.
+ * we can log in console,file,http services or in a database.
+ * winstion library by deafult logs messeges into console but we can configure it to log messeges anywere.
+ * Here we will configure it to log messeges in to a log file.
+ */
+app.use(express.json())  
 app.use('/api/courses',courses)
 
 app.get('/api/welcome',(req,res)=>{
     //throw new Error()
+    logger.info("request receives",req)
     res.send({
         messege:'Welcome to NodeJs'
     })
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,3 +1,17 @@
+const winston=require('winston')
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  })
 module.exports=(err,req,res,next)=>{
+    logger.error("Someting Went Wrong",err)
+    /**
+     * we have different methods for logging messeges like info,error,warn,debug,silly,verbose.
+     */
     res.status(500).send('Something Went wrong')
 }
